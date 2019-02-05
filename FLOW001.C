c44c1c72ade4a957daeb775d488abd134eefbb5b
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-02-06 00:16:29 +0530

    ADD TWO NUMBER FLOW001

---
+++ FLOW001.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int A,B,T,sum,i;
+	scanf("%d",&T);
+for(i=0;i<T;i++)
+{
+sum=0;
+scanf("%d %d",&A,&B);
+sum=A+B;
+printf("%d \n",sum);
+    
+}
+	return 0;
+}
+
