bf2f3d0bb72d0344060186996e115db16fdd6b7e
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-05-18 22:28:16 +0530

    Create MUFFINS3.C

---
+++ MUFFINS3.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	
+int T,i;
+scanf("%d",&T);
+for(i=0;i<T;i++){
+    
+    unsigned long int a;
+    scanf("%ld",&a);
+    printf("%lu\n",a/2+1);
+}
+
+	//return 0;
+}
+
