47f372a3062a7e25eb26473634a175a5f22291d1
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-05-18 22:31:03 +0530

    Create FLOW008.C

---
+++ FLOW008.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int T,i,n;
+scanf("%d",&T);
+while(T>0)
+{
+scanf("%d",&n);
+if(n<10)
+{
+    printf("What an obedient servant you are!\n");
+}
+else{
+    printf("-1\n");
+}
+T--;
+}
+	return 0;
+}
+
